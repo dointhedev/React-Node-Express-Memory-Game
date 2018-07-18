c61fff944137971ce0bbc5b8689de382f3eb6f6c
Avi Davis Mac <oodavis.avi@gmail.com>
2018-07-18 15:39:47 -0700

    Fixed up a bit of the responsiveness.

--- client/src/components/Jumbotron/JumbotronCont.js
+++ client/src/components/Jumbotron/JumbotronCont.js
@@ -6,7 +6,7 @@ const JumboTron = props => (
             <div>
                 <Jumbotron className="jumbo" fluid>
                     <Container className="jumbcont text-center" fluid>
-                        <h1 className="display-3">Clicky Game!</h1>
+                        <h1 className="display-4">Clicky Game!</h1>
                         <p className="lead">Click on an image to earn points, but don't click on any more than once!</p>
                     </Container>
                 </Jumbotron>
--- client/src/components/NavBar/NavBarCont.js
+++ client/src/components/NavBar/NavBarCont.js
@@ -6,12 +6,12 @@ const NavBar = props => (
     <div>
         <Container color="info" className="topbar px-4 py-3" fluid>
             <Row>
-                <Col md="3">
+                <Col md="3" className="text-center text-md-left">
                     Clicky React Game</Col>
                 <Col md="6" className="midtop">
                     Click On An Image To Start
                 </Col>
-                <Col md="3">
+                <Col md="3" className="text-center text-md-left">
                     Score: {props.count} | Top Score: {props.topScore}
                 </Col>
             </Row>
